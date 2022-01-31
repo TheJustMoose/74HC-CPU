24b6458e07319e8eaa1a09f39c6f09ab588bd604
Rybakov Vladimir <v.rybakov@corp.mail.ru>
2022-01-31 21:26:43 +0300

    Something work

--- soft/asm/asm.py
+++ soft/asm/asm.py
@@ -1,6 +1,8 @@
 import sys
 import os
 
+glabels = []
+
 # available command
 # name | code | ctype | args
 cmds = {
@@ -73,7 +75,6 @@ dst_regs = {
 "R7": 7,
 }
 
-
 def to_num(val):
   res = 0
   if val.find("0X") == 0 and len(val) > 2:
@@ -112,13 +113,14 @@ class Instruction:
 
   ctype = 0
   cop = 0
-  label = ""
+  arg_num = 0
 
+  label = ""
   error = ""
 
   def __init__(self, parts):
     if len(parts) == 0:
-      self.error = "No instruction found in: " + text
+      self.err("No instruction found in: " + text)
       return
 
     self.name = parts[0]
@@ -130,11 +132,12 @@ class Instruction:
       return
 
     if cmd["ARGS"] != len(parts) - 1:
-      self.error = "Error. Wait for %d args in %s" % (cmd["ARGS"], self.name)
+      self.err("Error. Wait for %d args in %s" % (cmd["ARGS"], self.name))
       return
 
     self.ctype = cmd["TP"]
     self.cop = cmd["COP"]
+    self.arg_num = cmd["ARGS"]
 
   def cut(self, s):
     return s if len(s) <= 5 else s[:4] + "~"
@@ -153,15 +156,53 @@ class Instruction:
   def is_valid(self):
     return len(self.error) == 0
 
+  def err(self, e):
+    if self.error:
+      self.error += " / "
+    self.error += e
+
   def get_size(self):
     return 1
+
+  def get_cmd(self):
+    res = self.name
+    if self.dst:
+      res += " " + self.dst
+    if self.src:
+      res += " " + self.src
+    return res
 
   def need_align(self, addr):
     return (self.get_size() == 2) and (addr % 2)
 
+  def to_bin(self, val, dig):
+    fmt = '0' + str(dig) + 'b'
+    return format(val, fmt)
+
+  def out(self, code):
+    print("     " + to_bin(code, 12))
+
 class ArithmInstruction(Instruction):
   def __init__(self, parts):
     super().__init__(parts)
+    if self.arg_num > 0:
+      if self.dst not in dst_regs:
+        self.err("Unknown register %s in '%s'" % (self.dst, self.get_cmd()))
+        self.err("Plz use this registers: %s" % (" ".join(dst_regs)))
+      if self.arg_num == 1 and not self.src:  # unary command with 1 arg
+        if self.dst not in src_regs:
+          self.err("Plz use this registers R0, R1, R2, R3 or use cmd with suffix 2")
+
+    if self.arg_num > 1:
+      if self.src in src_regs or is_num(self.src):
+        # src is ok
+        if is_num(self.src) and get_num(self.src) > 255:
+          self.err("Too big value %s in '%s'" % (self.src, self.get_cmd()))
+      elif self.src not in src_regs:
+        self.err("Unknown register %s in '%s'" % (self.src, self.get_cmd()))
+        self.err(" Plz use this registers: %s" % (" ".join(src_regs)))
+      elif not is_num(src):
+        self.err("Unknown value %s in '%s'" % (self.src, self.get_cmd()))
 
   def get_size(self):
     if is_num(self.src):
@@ -169,9 +210,28 @@ class ArithmInstruction(Instruction):
     else:
       return 1
 
+  def make_command(self):
+    res = 0
+    res |= ctype << 10
+    res |= dst << 7
+    res |= src << 4
+    res |= cop
+    out(res)
+
 class JumpInstruction(Instruction):
   def __init__(self, parts):
     super().__init__(parts)
+    if self.arg_num > 0:
+      if is_num(self.dst) and get_num(self.dst) > 65535:
+        self.set_err("Too big address %s in '%s'" % (self.dst, self.get_cmd()))
+
+  def is_valid(self):
+    if self.arg_num == 1:
+      if not is_num(self.dst) and self.dst not in glabels:
+        self.err("Unknown label %s in '%s'" % (self.dst, self.get_cmd()))
+        self.err("Plz use address or label")
+        return False
+    return True
 
   def get_size(self):
     if self.name == "RET" or self.name == "IRET" or self.name == "SPE":
@@ -189,6 +249,13 @@ class ControlInstruction(Instruction):
 class TransferInstruction(Instruction):
   def __init__(self, parts):
     super().__init__(parts)
+    if self.name == "LOAD" or self.name == "STORE":
+      if self.dst not in dst_regs:
+        self.err("Memory command require register. Here: %s" % (get_cmd()))
+    elif self.name == "CFG":
+      if self.src not in src_regs:
+        self.err("Unknown register %s in '%s'" % (src, get_cmd()))
+        self.err("Plz use this registers: %s" % (" ".join(src_regs)))
 
   def get_size(self):
     if self.name == "LOAD" or self.name == "STORE" or self.name == "IN":
@@ -203,8 +270,7 @@ class TransferInstruction(Instruction):
 class UnknownInstruction(Instruction):
   def __init__(self, parts):
     super().__init__(parts)
-    self.error = "Error. Unknown command: " + self.name
-
+    self.err("Error. Unknown command: " + self.name)
 
 def CreateInstruction(parts):
   ctype = -1
@@ -212,7 +278,6 @@ def CreateInstruction(parts):
     cmd = cmds[parts[0]]
     ctype = cmd["TP"]
   except KeyError:
-    #print("Key error: " + parts[0])
     pass
 
   if ctype == 0:
@@ -225,69 +290,6 @@ def CreateInstruction(parts):
     return TransferInstruction(parts)
   else:
     return UnknownInstruction(parts)
-
-
-label_to_line = {}
-label_to_addr = {}
-line_to_label = {}
-parsed_cmd = []
-have_errors = False
-res_bin = []
-
-def cmd_ok(parts):
-  name = parts[0]
-  dst = parts[1] if len(parts) > 1 else ""
-  src = parts[2] if len(parts) > 2 else ""
-
-  cmd = cmds[name]
-  args = cmd["ARGS"]
-  ct = cmd["TP"]  # command type
-  if len(parts) - 1 != args:
-    print("Error. Wait for %d args in %s" % (args, name))
-    return False
-
-  if ct == 0:  # arithm
-    if args > 0:
-      if dst not in dst_regs:
-        print("Unknown register %s in '%s'" % (dst, " ".join(parts)))
-        print(" Plz use this registers: %s" % (" ".join(dst_regs)))
-        return False
-      if args == 1 and len(parts) == 2:  # unary command with 1 arg
-        if dst not in src_regs:
-          print("Plz use this registers R0, R1, R2, R3 or use cmd with suffix 2")
-          return False
-    if args > 1:
-      if src in src_regs or is_num(src):
-        # src is ok
-        if is_num(src) and get_num(src) > 255:
-          print("Too big value %s in '%s'" % (src, " ".join(parts)))
-          return False
-      elif src not in src_regs:
-        print("Unknown register %s in '%s'" % (src, " ".join(parts)))
-        print(" Plz use this registers: %s" % (" ".join(src_regs)))
-        return False
-      elif not is_num(src):
-        print("Unknown value %s in '%s'" % (src, " ".join(parts)))
-        return False
-  elif ct == 1:  # jumps
-    if args > 0:
-      if dst in label_to_line or is_num(dst):
-        if is_num(dst) and get_num(dst) > 65535:
-          print("Too big address %s in '%s'" % (dst, " ".join(parts)))
-          return False
-      elif dst not in label_to_line:
-        print("Unknown label %s in '%s'" % (dst, " ".join(parts)))
-        print(" Plz use address or label")
-        return False
-      elif not is_num(dst):
-        print("Unknown value %s in '%s'" % (dst, " ".join(parts)))
-        return False
-  elif ct == 3:  # transfer
-    if name == "LOAD" or name == "STORE":
-      if dst not in dst_regs:
-        print("Memory command require register. Here: %s" % (" ".join(parts)))
-
-  return True
 
 def read_and_prepare(fname):
   res = []
@@ -325,7 +327,7 @@ def extract_labels_and_code(lines):
   for line in lines:
     if line.find(':') != -1:
       label = line[0:-1]  # remove colon
-      if label in label_to_line:
+      if label in glabels:
         print("Error. Label %s already exist." % label)
       else:
         last_label = label
@@ -333,6 +335,7 @@ def extract_labels_and_code(lines):
       i = CreateInstruction(line.split())
       if len(last_label):
         i.label = last_label
+        glabels.append(last_label)
         last_label = ""
       res.append(i)
 
@@ -347,34 +350,19 @@ def fix_alignment(instructions):
       addr +=1
       if i.need_align(addr):
         print("ASSEMBLER ERROR! HALT!!")
+        return
     addr += i.get_size()
     res.append(i)
   return res
 
-def calc_addr():
+def calc_labels_addr(instructions):
+  res = {}
   addr = 0
-  for idx, p in enumerate(parsed_cmd):
-    if idx in line_to_label:
-      lab = line_to_label[idx]
-      label_to_addr[lab] = addr
-    addr += cmd_size(p)
-
-def to_bin(val, dig):
-  fmt = '0' + str(dig) + 'b'
-  return format(val, fmt)
-
-def out(code):
-  print("     " + to_bin(code, 12))
-  res_bin.append(to_bin(code, 12))
-  return
-
-def make_command(ctype, cop, dst, src):
-  res = 0
-  res |= ctype << 10
-  res |= dst << 7
-  res |= src << 4
-  res |= cop
-  out(res)
+  for i in instructions:
+    if i.label:
+      res[i.label] = addr
+    addr += i.get_size()
+  return res
 
 def make_jump(ctype, cop, addr):
   res = 0
@@ -433,14 +421,17 @@ def asm(fname):
     return
 
   instructions = extract_labels_and_code(lines)
+  global glabels
   instructions = fix_alignment(instructions)
+  glabels = calc_labels_addr(instructions)
   addr = 0
   for i in instructions:
     i.log(addr)
     addr += i.get_size()
 
-  #calc_addr()
-  #print("label_to_addr: ", label_to_addr)
+  for l in glabels:
+    print("%04X %s" % (glabels[l], l))
+
   #assemble()
 
   #print("Machine code:")
@@ -454,8 +445,5 @@ def main():
     print("To use type:\nasm.py your_source.asm\n")
     return
   asm(sys.argv[1])
-
-  #print("labels: ", label_to_line)
-  #print("line_to_label: ", line_to_label)
 
 main()
