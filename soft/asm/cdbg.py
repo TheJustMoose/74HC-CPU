1b797362a38f11bd43b6283e7bfe0a08f9b24e74
Rybakov Vladimir <v.rybakov@corp.mail.ru>
2022-02-02 13:28:35 +0300

    Try to create debugger

--- soft/asm/cdbg.py
+++ soft/asm/cdbg.py
@@ -5,12 +5,72 @@ from cpu import *
 
 cop_to_name = {}
 
+cop_mask = 0x0F
+src_mask = 0x07 << 4
+dst_mask = 0x07 << 7
+ctype_mask = 0x03 << 10
+
+def arithm_cmd():
+  return 0
+
+def jump_cmd():
+  return 1
+
+def ctrl_cmd():
+  return 2
+
+def transfer_cmd():
+  return 3
+
+def get_ctype(w):
+  res = (w & ctype_mask) >> 10
+  return res
+
+def get_dst(w):
+  res = (w & dst_mask) >> 7
+  return res
+
+def get_src(w):
+  res = (w & src_mask) >> 4
+  return res
+
+def get_cop(w):
+  res = (w & cop_mask)
+  return res
+
 def extract_cops():
   for c in cmds:
     cmd = cmds[c]
     uid = (cmd["TP"] << 10) + cmd["COP"]
     cop_to_name[uid] = c
     print(uid, cmd)
+
+def get_cmd_size(w):
+  ctype = get_ctype(w)
+  cop = get_cop(w)
+  name = cop_to_name[cop]
+  if ctype == arithm_cmd():  # arithm instruction with const?
+    return 2 if get_src(w) == src_regs["CONST"] else 1
+
+  if ctype == jump_cmd():
+    if name == "RET" or name == "IRET" or name == "CSIE":
+      return 1
+    else:
+      return 2
+
+  if ctype == ctrl_cmd():
+    return 1
+
+  if ctype == transfer_cmd():
+    if name == "LOAD" or name == "IN":
+      return 1
+    elif name == "CFG":
+      return 2
+    else:  # STORE/OUT/XOUT
+      if get_src(w) == src_regs["CONST"]:
+        return 2
+      else:
+        return 1
 
 def disasm(w):
   cop = w & 0x0F
@@ -19,8 +79,11 @@ def disasm(w):
   return name
 
 def dbg(bin):
-  for b in bin:
-    print("%03X %4s" % (b, disasm(b)))
+  addr = 0
+  while addr < len(bin):
+    w = bin[addr]
+    print("%03X %4s" % (w, disasm(w)))
+    addr += get_cmd_size(w)
   return
 
 def load(fname):
