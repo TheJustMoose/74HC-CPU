1830371e7e026736924649fcb376b5783a88a2e6
TheJustMoose <vmorkva@ya.ru>
2024-03-10 21:24:24 +0300

    ASM in work: add tests for line strip

--- soft/asm/build_ut.bat
+++ soft/asm/build_ut.bat
@@ -1,2 +1,2 @@
 del unit_tests.exe
-"C:\Program Files\LLVM\bin\clang.exe" -std=c++17 -I doctest\doctest unit_tests.cc -o unit_tests.exe 2>err.txt
+"C:\Program Files\LLVM\bin\clang.exe" -std=c++17 -I doctest\doctest unit_tests.cc preprocessor.cc -o unit_tests.exe 2>err.txt
--- soft/asm/unit_tests.cc
+++ soft/asm/unit_tests.cc
@@ -1,6 +1,30 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 
+#include "preprocessor.h"
+
 TEST_CASE("check preprocessor cleaner") {
-  CHECK(false);
+  Preprocessor pre;
+  CHECK(pre.StripLine("") == "");
+  CHECK(pre.StripLine(" ") == "");
+  CHECK(pre.StripLine("  ") == "");
+
+  CHECK(pre.StripLine("a") == "a");
+  CHECK(pre.StripLine(" a") == "a");
+  CHECK(pre.StripLine("a ") == "a");
+  CHECK(pre.StripLine(" a ") == "a");
+  CHECK(pre.StripLine("  a  ") == "a");
+
+  CHECK(pre.StripLine("a b") == "a b");
+  CHECK(pre.StripLine("a  b") == "a b");
+  CHECK(pre.StripLine("a   b") == "a b");
+  CHECK(pre.StripLine(" a b") == "a b");
+  CHECK(pre.StripLine("a b ") == "a b");
+  CHECK(pre.StripLine("  a b  ") == "a b");
+
+  CHECK(pre.StripLine("label: mov  ax, bx  ; test cmd") == "label: mov ax, bx");
+  CHECK(pre.StripLine("  LSR   R0  ") == "LSR R0");
+
+  CHECK(pre.StripLine(".org 100h") == ".org 100h");
+  CHECK(pre.StripLine("  .org 100h ; comment") == ".org 100h");
 }
