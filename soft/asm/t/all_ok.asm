68d8fe2b6fbd4041de5ef4d76edee79d9ef9eac1
TheJustMoose <vmorkva@ya.ru>
2024-03-11 19:59:31 +0300

    ASM in work: test file

---
+++ soft/asm/t/all_ok.asm
@@ -0,0 +1,81 @@
+.def A r0
+.def CNT 10
+
+begin:
+entry:
+clr  r0
+inc  r0
+incc r1
+add  r0, r1
+addc r1, r2
+and  r2, r3
+or   r3, r4
+xor  r4, r5
+mul  r5, r6
+mul  r6, CNT
+inv  r7
+swap r0
+lsr  r1
+lsrc r2
+mov  r1, r0
+lpm  r2, XI
+lpmw r2, XI  ; r3:r2 := *X++
+ld   r3, Y
+ld   r3, YD - 4
+ld   r3, YI + 4
+in   r4, pin0
+out  port0, r5
+togl port1, r7
+st   ZI, r6
+cmp  r7, -5
+cmpc r7, 0
+call proc
+jz   begin
+jl   begin
+jne  begin
+je   begin
+jg   begin
+jc   begin
+jnz  begin
+jnc  begin
+jhc  begin
+jnhc begin
+stop
+afcall proc2
+jmp  begin
+
+nop
+
+proc:
+xor r0, r0
+ret
+
+.org 100h
+proc2:
+add r0, r0
+add r0, 10
+ret
+
+mov r1 r2
+nop
+
+push r0
+pop  r1
+
+mov  YL, LO(Hello)
+mov  YH, HI(Hello)
+
+end:
+jmp end
+
+;.org 1000h
+
+.str Hello "Test string!"
+.str Menu  "Menu"
+
+.def CLR(reg) XOR reg, reg
+.def INC(reg) ADD reg, 1
+.def INCC(reg) ADDC reg, 0
+.def TEST()  STOP
+.def PUSH(r) ST SPD, r
+.def POP(r)  LD r, SPI - 1
