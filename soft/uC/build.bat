b2b5e873d4aec143355ed5d55c2433ba804dcbd9
TheJustMoose <vmorkva@ya.ru>
2024-03-28 22:54:57 +0300

    uC in work: fix build.bat

--- soft/uC/build.bat
+++ soft/uC/build.bat
@@ -1,2 +1,3 @@
 del 74hc-uC.exe
-"C:\Program Files\LLVM\bin\clang.exe" -std=c++17 main.cc parser.cc lexer.cc token.cc word.cc type.cc -o 74hc-uC.exe 2>err.txt
+"C:\Program Files\LLVM\bin\clang.exe" -std=c++17 ^
+ main.cc num.cc real.cc lexer.cc parser.cc token.cc word.cc type.cc -o 74hc-uC.exe 2>err.txt
