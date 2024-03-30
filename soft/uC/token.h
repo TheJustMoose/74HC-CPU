9628eb7d752e16f812ec96d676e5becd88202762
TheJustMoose <vmorkva@ya.ru>
2024-03-30 21:15:42 +0300

    uC in work: moving Word class, fix .gitignore

--- soft/uC/token.h
+++ soft/uC/token.h
@@ -38,3 +38,31 @@ class Real : public Token {
  private:
   float value_ {0};
 };
+
+class Word: public Token {
+ public:
+  Word(std::string s, Tag tag)
+    : Token(tag), lexeme_(s) {}
+
+  std::string toString() override {
+    return lexeme_;
+  }
+
+  std::string lexeme() {
+    return lexeme_;
+  }
+
+ private:
+  void Init();
+
+  std::string lexeme_ {};
+};
+
+class Type: public Word {
+ public:
+  Type(std::string s, Tag tag, int w)
+    : Word(s, tag), width_(w) {}
+
+ private:
+  int width_ {0};
+};
