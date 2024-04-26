3a45acedc4b1e79cded63a0d673d35d92b0b6ad3
TheJustMoose <vmorkva@ya.ru>
2024-04-26 23:51:44 +0300

    Playing with tkinter

--- soft/dgui.py
+++ soft/dgui.py
@@ -2,6 +2,7 @@
 #
 
 import tkinter as tk
+from PIL import Image, ImageTk
 
 window = tk.Tk()
 greeting = tk.Label(text="Hello, Tkinter")
@@ -10,6 +11,15 @@ label = tk.Label(text="Hello, Tkinter", background="#34A2FE")
 label.pack()
 button = tk.Button(text="Click me!", width=25, height=5, bg="blue", fg="yellow")
 button.pack()
+
+# Load and display an image
+image = Image.open('cat-fish.jpg')
+image = ImageTk.PhotoImage(image)
+
+# Create a label to display the image
+image_label = tk.Label(window, image=image)
+image_label.pack()
+
 entry = tk.Entry()
 entry.pack()
 window.mainloop()
