544ce35264e8f688a344ace322648893b6a8ad7d
TheJustMoose <justmoose@ya.ru>
2025-07-27 21:34:34 +0300

    Compiler in work

--- soft/uC/logical.h
+++ soft/uC/logical.h
@@ -4,5 +4,24 @@
 
 class Logical: public Expr {
  public:
-  Logical(Token* tok, Expr* x1, Expr* x2);
+  Logical(Token* tok, Expr* x1, Expr* x2)
+   : Expr(tok, nullptr) {                      // null type to start
+    expr1_ = x1;
+    expr2_ = x2;
+    type_ = check(expr1_->type(), expr2_->type());
+    if (type_ == nullptr)
+      error("type error");
+  }
+
+  Type* check(Type* p1, Type* p2) {
+    if (p1 == Type::Bool() && p2 == Type::Bool())
+      return Type::Bool();
+    else
+      return nullptr;
+  }
+
+
+ public:
+  Expr* expr1_ { nullptr };
+  Expr* expr2_ { nullptr };
 };
