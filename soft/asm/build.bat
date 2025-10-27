105d9df41022f111428615cf2c435c55451a53fb
TheJustMoose <justmoose@ya.ru>
2025-10-27 21:41:41 +0300

    Convert build system to cmake

--- soft/asm/build.bat
+++ soft/asm/build.bat
@@ -1,2 +1,4 @@
-del 74hc-asm.exe
-"C:\Program Files\LLVM\bin\clang.exe" -std=c++17 assm.cc file_reader.cc main.cc preprocessor.cc str_util.cc -o 74hc-asm.exe 2>err.txt
+md bin
+cd bin
+cmake ..
+cmake --build . --config Release
--- soft/asm/build_ut.bat
+++
@@ -1,2 +0,0 @@
-del unit_tests.exe
-"C:\Program Files\LLVM\bin\clang.exe" -std=c++17 -I doctest\doctest assm.cc file_reader.cc preprocessor.cc str_util.cc unit_tests.cc -o unit_tests.exe 2>err.txt
